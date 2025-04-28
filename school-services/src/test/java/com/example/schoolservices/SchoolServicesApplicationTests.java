98e4ba705457a2d5689940c9b54c88278df9264b
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2025-04-28 17:37:01 +0530

    Initial commit

---
+++ school-services/src/test/java/com/example/schoolservices/SchoolServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
